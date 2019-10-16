4ce1bced143c948328e0205500454ac6f7bb8247
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-16 13:27:12 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,5 @@ variable "sonarvpc" {}
 variable "sonarsub" {}
 // variable "elk_instance_name" {}
 variable "instance_name" {}
+variable "sonarqube_instance_ip_name" {}
+variable "sonarqube_instance_ip_region" {}
